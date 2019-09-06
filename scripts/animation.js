84710878ffdc12c75a99cf7496c9f62669c7e474
IOR-MNS <52220546+IOR-MNS@users.noreply.github.com>
2019-09-07 02:07:19 +0900

    update

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,14 +1,1 @@
-function animateLines(parentElement, interval)
-{
-	var children = parentElement.childNodes;
-	var i = 0;
-	
-	setTimeout( function step_animateLines() {
-		if (i >= children.length) {
-			return;
-		}
-		children[i].style.display = 'inline';
-		setTimeout(step_animateLines, interval);
-		i++;
-	}, interval);
-}
+window.animateLines=function(d,b){var c=d.childNodes,a=0;setTimeout(function e(){a>=c.length||(c[a].style.display="inline",setTimeout(e,b),a++)},b)};
