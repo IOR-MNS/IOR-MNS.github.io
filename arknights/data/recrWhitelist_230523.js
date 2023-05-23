019e6b058a2d257957fa2054160cd1dd51704543
IOR-MNS <52220546+IOR-MNS@users.noreply.github.com>
2023-05-23 13:47:09 +0900

    명일방주 공개모집 리스트 갱신
    위디, 엘리시움, 츠키노기, 아스베스토스 추가

--- arknights/data/recrWhitelist_230523.js
+++ arknights/data/recrWhitelist_230523.js
@@ -2,11 +2,11 @@
 var recrWhiteList = {}
 recrWhiteList.version = {} // 서버별 DB의 버전 표기는 loadRecr.js의 configSelectionPanel()에서 담당함.
 
-// 마지막 확인: 2023-01-13 16:00
+// 마지막 확인: 2023-05-23 16:00
 // 한, 일, 글섭
 recrWhiteList.version.en = recrWhiteList.version.jp = recrWhiteList.version.kr = "2023.01.13"
-recrWhiteList.en = recrWhiteList.jp = recrWhiteList.kr = 
-["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03","KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62","PA63","PA64","PA65","PL02","PL03","PL04","R100","R104","R105","R106","R107","R108","R119","R122","R123","R128","R303","RB02","RCX2","RCX3","RCX4","RL01","RL02","RL03","RL04","RL05","RL06","RR01","SG01","SI01","SR03","SS02","ST01","ST02","ST03","SW01","SW02","US10","USS1","USS2","USS3","VC01","VC02","VC03","VC04","YD01","RE41","R110","HT03","SR27","LT02","LM20","LM19","KZ15","R155","YD05","R160","RV02","R158","HK03","R109","R148","R144"]
+recrWhiteList.en = recrWhiteList.jp = recrWhiteList.kr = ["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03","KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62","PA63","PA64","PA65","PL02","PL03","PL04","R100","R104","R105","R106","R107","R108","R119","R122","R123","R128","R303","RB02","RCX2","RCX3","RCX4","RL01","RL02","RL03","RL04","RL05","RL06","RR01","SG01","SI01","SR03","SS02","ST01","ST02","ST03","SW01","SW02","US10","USS1","USS2","USS3","VC01","VC02","VC03","VC04","YD01","RE41","R110","HT03","SR27","LT02","LM20","LM19","KZ15","R155","YD05","R160","RV02","R158","HK03","R109","R148","R144"]
+
 
 // 마지막 확인: 2021-09-29 18:36
 // 중섭
@@ -19,6 +19,9 @@ recrWhiteList.cn =
 
 /*
 // 한, 일, 글섭
+// 2023-01-13 16:00 갱신 시점(2023-05-23 갱신 이전)
+["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03","KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62","PA63","PA64","PA65","PL02","PL03","PL04","R100","R104","R105","R106","R107","R108","R119","R122","R123","R128","R303","RB02","RCX2","RCX3","RCX4","RL01","RL02","RL03","RL04","RL05","RL06","RR01","SG01","SI01","SR03","SS02","ST01","ST02","ST03","SW01","SW02","US10","USS1","USS2","USS3","VC01","VC02","VC03","VC04","YD01","RE41","R110","HT03","SR27","LT02","LM20","LM19","KZ15","R155","YD05","R160","RV02","R158","HK03","R109","R148","R144"]
+
 // 2022-07-29 16:00 갱신 이전
 ["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03",
 "KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62",
@@ -120,12 +123,12 @@ if (!updateDone || Date.now() <= (updateTime.us.getTime() + 24*60*60*1000)) {
 
 // 정해진 시각에 DB 자동 업데이트하도록 하는 코드
 // 한, 일, 글섭 1/14 16시부터 공개모집 풀 추가. 한, 일섭은 UTC+9 기준, 글섭은 UTC-7 기준.
-var updateData = ["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03","KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62","PA63","PA64","PA65","PL02","PL03","PL04","R100","R104","R105","R106","R107","R108","R119","R122","R123","R128","R303","RB02","RCX2","RCX3","RCX4","RL01","RL02","RL03","RL04","RL05","RL06","RR01","SG01","SI01","SR03","SS02","ST01","ST02","ST03","SW01","SW02","US10","USS1","USS2","USS3","VC01","VC02","VC03","VC04","YD01","RE41","R110","HT03","SR27","LT02","LM20","LM19","KZ15","R155","YD05","R160","RV02","R158","HK03","R109","R148","R144"]
-
+var updateData = ["A41","A42","A43","A44","AA01","AA02","AA03","AZ01","BS02","BS03","BS04","FO01","FO02","FO03","GG01","GG03","HK01","IU04","IU05","JC01","JC03","JC05","JC06","KZ03","KZ04","LM04","LM05","LM08","LM10","LM11","LM12","LN01","LT01","LT05","LT77","MN02","MN03","PA11","PA12","PA13","PA14","PA15","PA41","PA42","PA43","PA44","PA61","PA62","PA63","PA64","PA65","PL02","PL03","PL04","R100","R104","R105","R106","R107","R108","R119","R122","R123","R128","R303","RB02","RCX2","RCX3","RCX4","RL01","RL02","RL03","RL04","RL05","RL06","RR01","SG01","SI01","SR03","SS02","ST01","ST02","ST03","SW01","SW02","US10","USS1","USS2","USS3","VC01","VC02","VC03","VC04","YD01","RE41","R110","HT03","SR27","LT02","LM20","LM19","KZ15","R155","YD05","R160","RV02","R158","HK03","R109","R148","R144","R117","R131","R129","RB05"]
 var updateTime = {
-    kr: new Date(Date.UTC(2023, 0, 13, 7)), // month is 0-indexed
-    us: new Date(Date.UTC(2023, 0, 13, 23))
+    kr: new Date(Date.UTC(2023, 4, 23, 7)), // month is 0-indexed
+    us: new Date(Date.UTC(2023, 4, 23, 23))
 }
+var updateVersion = "2023.05.23"
 var updateTimeStr = {}
 
 for (let region in updateTime) {
@@ -142,11 +145,13 @@ var DBUpdated = {
 if (Date.now() >= updateTime.kr) {
     DBUpdated.kr = true
     recrWhiteList.jp = recrWhiteList.kr = updateData
+    recrWhiteList.version.jp = recrWhiteList.version.kr = updateVersion
 }
 
 if (Date.now() >= updateTime.us) {
     DBUpdated.us = true
     recrWhiteList.en = updateData
+    recrWhiteList.version.en = updateVersion
 }
 
 let updateDone = true
